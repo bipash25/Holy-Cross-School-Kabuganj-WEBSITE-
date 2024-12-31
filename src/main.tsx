6ec06b03be47f6642d436977a0a9f2d42e391982
bipash25 <secondbipro@gmail.com>
2024-12-31 03:14:48 +0000

    fixes3

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
+// main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { TempoDevtools } from "tempo-devtools";
+import App from "./App";
+import "./index.css";
 
 TempoDevtools.init();
 
 const basename = import.meta.env.BASE_URL;
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
       <App />
--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 const conditionalPlugins = [];
-<<<<<<< HEAD
 if (process.env.TEMPO) {
   conditionalPlugins.push(["tempo-devtools/swc", {}]);
-=======
-
-if (process.env.TEMPO) {
-  conditionalPlugins.push(['tempo-devtools/swc', {}]);
->>>>>>> 8f7ab9677904b69c65fffc3f502542703c24b379
 }
 
 export default defineConfig({
   plugins: [
     react({
-<<<<<<< HEAD
       plugins: [...conditionalPlugins],
     }),
-=======
-      plugins: [...conditionalPlugins]
-    })
->>>>>>> 8f7ab9677904b69c65fffc3f502542703c24b379
   ],
   resolve: {
     alias: {
