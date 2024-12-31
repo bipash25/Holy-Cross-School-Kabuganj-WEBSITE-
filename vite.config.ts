056142b1d87083333ac148d8186b4597ea6232a0
bipash <111284780+bipash25@users.noreply.github.com>
2024-12-31 08:15:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,18 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const conditionalPlugins = [];
+
+if (process.env.TEMPO) {
+  conditionalPlugins.push(['tempo-devtools/swc', {}]);
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      plugins: [...conditionalPlugins]
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
